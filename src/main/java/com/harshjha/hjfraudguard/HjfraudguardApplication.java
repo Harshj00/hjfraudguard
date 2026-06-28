bd6f0f75262e88cb4757d033610a548bf1c88b33
Harsh Jha <harshjha082004@gmail.com>
2026-06-28 14:19:20 +0530

    Initial Spring Boot skeleton for HJFraudGuard

---
+++ src/main/java/com/harshjha/hjfraudguard/HjfraudguardApplication.java
@@ -0,0 +1,13 @@
+package com.harshjha.hjfraudguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HjfraudguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HjfraudguardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harshjha/hjfraudguard/HjfraudguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshjha.hjfraudguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HjfraudguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
